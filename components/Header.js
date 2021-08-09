71f68f72853884bb22eec5656915f677e5b912f3
Priyansh Shah <shah.priyansh04@gmail.com>
2021-08-09 15:54:46 +0530

    Fixed a bug where Dark Mode toggle was not appearing on some screens

--- components/Header.js
+++ components/Header.js
@@ -57,7 +57,7 @@ function Header({ placeholder }) {
   };
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 dark:bg-dark-primary ">
+    <header className="sticky  top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 dark:bg-dark-primary ">
       <div
         className="relative flex items-center h-10 cursor-pointer my-auto "
         onClick={() => router.push("/")}
@@ -69,7 +69,7 @@ function Header({ placeholder }) {
           objectPosition="left"
         />
       </div>
-      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+      <div className="hidden md:inline-flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
           value={searchInput}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400 dark:placeholder-dark-text dark:text-dark-text"
@@ -84,29 +84,28 @@ function Header({ placeholder }) {
           onClick={search}
         />
       </div>
-      <div className="flex items-center justify-end space-x-4 text-gray-500 dark:text-dark-text">
+      <div className="flex items-center justify-end -mx-24 md:mx-0 space-x-4 text-gray-500 dark:text-dark-text">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="hidden md:inline-flex h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
           <MenuIcon className="h-6 cursor-pointer" />
-          <UserCircleIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="hidden md:inline h-6 cursor-pointer" />
+          {theme === "light" ? (
+            <MoonIcon
+              onClick={() => {
+                changeTheme();
+              }}
+              className="h-7 cursor-pointer hover:scale-110 transform transition ease-in"
+            ></MoonIcon>
+          ) : (
+            <SunIcon
+              onClick={() => {
+                changeTheme();
+              }}
+              className="h-6 cursor-pointer hover:scale-110 transform transition ease-in"
+            ></SunIcon>
+          )}
         </div>
-
-        {theme === "light" ? (
-          <MoonIcon
-            onClick={() => {
-              changeTheme();
-            }}
-            className="h-6 cursor-pointer hover:scale-110 transform transition ease-in"
-          ></MoonIcon>
-        ) : (
-          <SunIcon
-            onClick={() => {
-              changeTheme();
-            }}
-            className="h-6 cursor-pointer hover:scale-110 transform transition ease-in"
-          ></SunIcon>
-        )}
       </div>
 
       {searchInput && (
